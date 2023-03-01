557bb8ca183edeb2725b9f35608b2a2655435cb8
Nuseyba <n.kenadiid@gmail.com>
2023-03-01 20:30:29 +0000

    created delete and post request for notes

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,3 +1,46 @@
 const path = require('path');
 const fs = require("fs")
 
+// "POST" request//
+
+module.exports = (app) => {
+    app.get("/api/notes", (req, res) => res.json(noteData));
+  
+    app.post("/api/notes", (req, res) => {
+      noteData.push(req.body);
+  
+      for (let i = 0; i < noteData.length; i++) {
+        noteData[i].id = i + 1;
+      }
+  
+      fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) => {
+        if (err) throw err;
+        console.log("Updated!");
+      });
+      res.json(true);
+    });
+
+    // "DELETE" request
+
+    app.delete("/api/notes/:id", (req, res) => {
+        const id = req.params.id;
+        let index;
+        for (let i in noteData) {
+          if (id === noteData[i].id) {
+            index = i;
+          }
+        }
+        noteData.splice(index, 1);
+        for (let i in noteData) {
+          noteData[i].id = i;
+        }
+        fs.writeFile(
+          path.join(__dirname, "../db/db.json"),
+          JSON.stringify(noteData),
+          (err) => {
+            if (err) throw err;
+          }
+        );
+        res.json(true);
+      });
+    };
--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+// Here are the dependencies //
+
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes.js');
 const htmlRoutes = require('./routes/htmlRoutes.js')
+
+// The asynchronous proceses is handled here //
+
+const readFileAsync =util.promisify(fs.readFile)
+const writeFileAsync=util.promisify(fs.WriireFile)
+
+// creating Port and setting up server //
+
+const PORT = process.env.PORT || 3001;
+
 
